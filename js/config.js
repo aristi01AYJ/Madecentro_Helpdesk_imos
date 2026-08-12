9fa88af21d008110bcf084f7e2439ebdf0d43ab6
Soporte IMOS Setup <setup@madecentro.local>
2026-08-11 19:42:50 -0500

    Configurar URL del flujo Mis Casos

--- js/config.js
+++ js/config.js
@@ -36,7 +36,7 @@ const APP_CONFIG = {
   // Pegá acá la URL "HTTP POST URL" que te da cada flujo al guardarlo.
   powerAutomate: {
     crearCasoUrl: "https://defaultc03f8e4026004b72bd823d604e8a9a.ac.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/18/workflows/36f266747c6044bb8289c03eb2702207/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=BB78BEJKqv4ZUz2sSl3kTcYBqRWT210wlMnH5Z_f0eM",
-    misCasosUrl: "PEGAR_URL_DEL_FLUJO_MIS_CASOS",
+    misCasosUrl: "https://defaultc03f8e4026004b72bd823d604e8a9a.ac.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/13/workflows/863e00d32d28466198405d7e03398a21/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=IHg6f3WT-5zTwi-PJlV1-UG8QLiRxHF7td4c5uxRk4A",
     casosResueltosUrl: "PEGAR_URL_DEL_FLUJO_CASOS_RESUELTOS",
   },
 
